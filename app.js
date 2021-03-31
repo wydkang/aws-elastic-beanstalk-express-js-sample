06704c9dbf1a7d3e485e7ca57208db8f6b834088
wydkang <wyd.kang@gmail.com>
2021-03-31 18:21:40 +0200

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello DK 2!'));
+app.get('/', (req, res) => res.send('Hello DK 3 revision!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
